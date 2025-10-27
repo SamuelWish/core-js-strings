f7336fad6609b76687157db5c3b8b66a742a2bef
samuelwish <viwnya08@gmail.com>
2025-10-28 02:12:57 +0300

    feat: implement reverseWords function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -355,8 +355,15 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const backwardWords = str
+    .split('')
+    .reverse()
+    .join('')
+    .split(' ')
+    .reverse()
+    .join(' ');
+  return backwardWords;
 }
 
 /**
